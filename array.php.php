48c236ba3bc1680410e0029eea3600a7f130330a
CHIROY <danielciroy87@gmail.com>
2023-10-10 12:30:30 +0200

    les tableaux en php

---
+++ array.php.php
@@ -0,0 +1,31 @@
+
+<?php
+// $jourssemaine=array('lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche');
+
+// foreach ($jourssemaine as $jour){
+//     echo "$jour <br/>";
+// }
+
+
+// for ($i=0; $i<count($jourssemaine); $i++){
+//     echo"$jourssemaine[$i] <br/>";
+// }
+
+
+$personne=array("paul"=>30, "daniel"=>50, "junior"=>60);
+
+foreach ($personne as $key => $value){
+    echo "$key a eu $value ans. <br/>";
+}
+
+
+ 
+
+?>
+
+
+
+
+
+
+
